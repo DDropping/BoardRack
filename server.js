54cbfc01b704446bc37e7d8cdebfbd68f7c26629
David Dropping <ddroppin@mail.sfsu.edu>
2019-05-26 23:18:21 -0700

    Create user auth messages posts route files

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
+//define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/messages', require('./routes/api/messages'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server connected to port: ${PORT}`));
