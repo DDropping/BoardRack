220cd6b0dd22a70425817c51ea6c4416b6cf72b3
David Dropping <ddroppin@mail.sfsu.edu>
2019-06-05 21:42:24 -0700

    Create api/posts/postID route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,12 +5,20 @@ const auth = require('../../middleware/auth');
 const Post = require('../../models/Post');
 const User = require('../../models/User');
 
-// @route   GET api/posts  ***NEEDS WORK / NOT FINISHED***
+// @route   GET api/posts/postID  ***NEEDS WORK / NOT FINISHED / NEEDS TO BE TESTED***
 // @desc    Get selected post
 // @access  Public
 router.get('/', async (req, res) => {
   try {
-    const post = await Post.findOne({});
+    const post = await Post.findOne({ post: req.post.id }).populate('user', [
+      'username'
+    ]);
+
+    if (!post) {
+      return res.status(400).json({ msg: 'There is no post with this id' });
+    }
+
+    res.json(post);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
