1d8311b879453f0b6be6b0a2bc7474fffe9bef62
David Dropping <ddroppin@mail.sfsu.edu>
2019-06-15 16:22:37 -0700

    Create api/posts/update/:id route to update an existing post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,7 +8,7 @@ const User = require('../../models/User');
 
 // @route   POST api/posts
 // @desc    Create a post
-// @access  Public
+// @access  Private
 router.post(
   '/',
   [
@@ -80,6 +80,96 @@ router.post(
       await post.save();
       res.json(post);
       console.log(post);
+    } catch (err) {
+      console.error(err.message);
+      re.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route   POST api/posts/update/:id ***might not be secure, need to test if anyone can change or just the origional poster***
+// @desc    Update a post
+// @access  Private
+router.post(
+  '/update/:id',
+  [
+    auth,
+    [
+      (check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+      check('price', 'Price is required')
+        .not()
+        .isEmpty())
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      isNewBoard,
+      title,
+      price,
+      shaper,
+      model,
+      boardType,
+      condition,
+      isWaterTight,
+      height,
+      width,
+      depth,
+      volume,
+      country,
+      state,
+      city,
+      zip,
+      description
+    } = req.body;
+
+    //build post object
+    const postFields = {};
+    postFields.user = req.user.id;
+    if (isNewBoard) postFields.isNewBoard = isNewBoard;
+    if (title) postFields.title = title;
+    if (price) postFields.price = price;
+    if (shaper) postFields.shaper = shaper;
+    if (model) postFields.model = model;
+    if (boardType) postFields.boardType = boardType;
+    if (condition) postFields.condition = condition;
+    if (isWaterTight) postFields.isWaterTight = isWaterTight;
+    if (description) postFields.description = description;
+
+    // //build dimensions object
+    postFields.dimensions = {};
+    if (height) postFields.dimensions.height = height;
+    if (width) postFields.dimensions.width = width;
+    if (depth) postFields.dimensions.depth = depth;
+    if (volume) postFields.dimensions.volume = volume;
+
+    // //build location object
+    postFields.location = {};
+    if (country) postFields.location.country = country;
+    if (state) postFields.location.state = state;
+    if (city) postFields.location.city = city;
+    if (zip) postFields.location.zip = zip;
+
+    try {
+      let post = await Post.findById(req.params.id);
+
+      if (post) {
+        // Update
+        post = await Post.findOneAndUpdate(
+          req.param.id,
+          { $set: postFields },
+          { new: true }
+        );
+
+        await post.save();
+        return res.json(post);
+      }
     } catch (err) {
       console.error(err.message);
       re.status(500).send('Server Error');
