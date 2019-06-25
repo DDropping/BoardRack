91ea5d3727a54b40708a86885514a74dac393b75
David Dropping <ddroppin@mail.sfsu.edu>
2019-06-24 17:33:30 -0700

    Delete test route for GET api/shops

--- routes/api/shops.js
+++ routes/api/shops.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   GET api/shops
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => {
-  res.send('Shops Route');
-});
-
 //***** NEEDS TO BE SECURED ********
 //does not include posts, can use populate to add in
 // @route   GET api/shops
