3eb407e9c38099e27f58077484fb9b9086e503aa
David Dropping <ddroppin@mail.sfsu.edu>
2019-06-24 17:59:26 -0700

    Change account creation from api/users to api/accounts

--- routes/api/accounts.js
+++ routes/api/accounts.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 //***** NEEDS TO BE SECURED ********
 //does not include posts, can use populate to add in
@@ -17,5 +24,70 @@ router.get('/', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route   POST api/accounts
+// @desc    register new account (username, email, password, userType)
+// @access  Public
+// validation checks >> return errors >> check if user exists >> create user instance >> encrypt password >> save to mongodb >> create jsonWebToken
+router.post(
+  '/',
+  [
+    check('username', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, email, password, userType } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      user = new User({
+        username,
+        email,
+        password,
+        userType
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
