28e9086f3512a083700c37b44c398385cc68605b
David Dropping <ddroppin@mail.sfsu.edu>
2019-06-24 17:31:18 -0700

    Create route api/shops to return all shop profiles

--- routes/api/shops.js
+++ routes/api/shops.js
@@ -8,4 +8,21 @@ router.get('/', (req, res) => {
   res.send('Shops Route');
 });
 
+//***** NEEDS TO BE SECURED ********
+//does not include posts, can use populate to add in
+// @route   GET api/shops
+// @desc    Get all shops
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await User.find({ userType: 'shop' }).select(
+      '-password -email -messageThreads'
+    );
+    res.json(profiles);
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
