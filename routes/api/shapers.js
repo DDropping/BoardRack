5f16650ad08ab18cbcdb921ebe7b40c12e3d9e1d
David Dropping <ddroppin@mail.sfsu.edu>
2019-06-24 17:32:47 -0700

    Create route api/shapers to retrieve all shaper profiles

--- routes/api/shapers.js
+++ routes/api/shapers.js
@@ -8,4 +8,21 @@ router.get('/', (req, res) => {
   res.send('Shapers Route');
 });
 
+//***** NEEDS TO BE SECURED ********
+//does not include posts, can use populate to add in
+// @route   GET api/shapers
+// @desc    Get all shapers
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await User.find({ userType: 'shaper' }).select(
+      '-password -email -messageThreads'
+    );
+    res.json(profiles);
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
