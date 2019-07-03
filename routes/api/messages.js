c979cfddc5f7c5c092cc5c5d19a6912662c9d3ab
David Dropping <ddroppin@mail.sfsu.edu>
2019-07-02 20:11:30 -0700

    Update messages.js import middleware and models

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const Message = require('../../models/Message');
+
 // @route   GET api/messages
-// @desc    Create message threat / add message to existing thread
+// @desc    Create message thread / add message to existing thread
 // @access  Private
 router.get('/', auth, (req, res) => {
   try {
