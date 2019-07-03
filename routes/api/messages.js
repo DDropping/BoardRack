9e99fc25ac8700fb520b4e4f5da508447131365c
David Dropping <ddroppin@mail.sfsu.edu>
2019-07-02 20:09:46 -0700

    Update messages route, start development on create message thread route

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 // @route   GET api/messages
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => {
-  res.send('Messages Route');
+// @desc    Create message threat / add message to existing thread
+// @access  Private
+router.get('/', auth, (req, res) => {
+  try {
+    res.send('Message');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json('Server Error');
+  }
 });
 
 module.exports = router;
