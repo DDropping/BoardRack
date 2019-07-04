65a6ed257b9e5c43ff0de18ca489426b7467ad9f
David Dropping <ddroppin@mail.sfsu.edu>
2019-07-03 18:05:08 -0700

    Update messages route

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -3,14 +3,24 @@ const router = express.Router();
 
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 const Message = require('../../models/Message');
 
+// !!! ROUTE NOT COMPLETE !!! ROUTE NOT COMPLETE !!!
 // @route   GET api/messages
 // @desc    Create message thread / add message to existing thread
 // @access  Private
-router.get('/', auth, (req, res) => {
+router.get('/:postId', auth, async (req, res) => {
   try {
-    res.send('Message');
+    //get related post
+    // const post = await Post.findOne({ postId: req.params.postId }).populate();
+    // res.json(post);
+
+    const post = await Post.findById(req.params.id).populate();
+    //get owner and buyer
+    //check if thread between users already exists for this post
+    //create new thread
+    //add message to thread
   } catch (err) {
     console.error(err.message);
     res.status(500).json('Server Error');
