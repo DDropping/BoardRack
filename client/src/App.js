933bda48e1541b764049557738626f666736978c
David Dropping <ddroppin@mail.sfsu.edu>
2019-07-08 17:13:04 -0700

    Create login and register routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/layout/home';
+import Home from './components/layout/Home';
+import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => {
   return (
     <Router>
       <Fragment>
-        <Route exact path='/' component={Home} />
+        <Navbar />
+        <section className='container'>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </section>
       </Fragment>
     </Router>
   );
