19d81c7c64b74cdf9932195054ddfbcd897b3cd9
David Dropping <ddropping@gmail.com>
2019-08-07 15:53:23 -0700

    Create store and add to provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import store from './store';
 import App from './components/App';
 import Home from './components/Home';
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App>
         <Route path="/" exact component={Home} />
