81b20ee73ba44313df095485c509f028eda25461
David Dropping <ddropping@gmail.com>
2019-08-08 15:57:53 -0700

    Add button component from antd to homecomponent

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Button from 'antd/es/button';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <div>Home Page</div>
+        <div className="App">
+          <Button type="primary">Button</Button>
+        </div>
       </div>
     );
   }
