7d2c0526b9da43ff24a4211fe4175fbef7edeb2b
David Dropping <ddropping@gmail.com>
2019-08-13 13:35:34 -0700

    Create alert reducer, type, and add to combine reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-
-const tempReducer = (state, action) => {
-  return 'Hello';
-};
+import alert from './alert';
 
 export default combineReducers({
-  tempReducer
+  tempReducer,
+  alert
 });
