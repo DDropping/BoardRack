3888083f4ad296edd7bde24ff8d097dac126afe5
David Dropping <ddropping@gmail.com>
2019-08-14 13:15:22 -0700

    Remove searchbar functionality in navbar

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,2 @@
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
-
-export const OPEN_DRAWER = 'OPEN_DRAWER';
-export const CLOSE_DRAWER = 'CLOSE_DRAWER';
--- client/src/components/navigation/NavBar.js
+++ client/src/components/navigation/NavBar.js
@@ -1,8 +1,7 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { Drawer, Button } from 'antd';
 import RightMenu from './RightMenu';
 import RightMenuDrawer from './RightMenuDrawer';
-import SearchBar from './SearchBar';
 import './navbar.css';
 
 class NavBar extends Component {
@@ -23,43 +22,31 @@ class NavBar extends Component {
 
   render() {
     return (
-      <Fragment>
-        <nav className="menuBar">
-          <div className="logo">
-            <img
-              alt="logo"
-              src={process.env.PUBLIC_URL + '/images/br_logo_xs.jpg'}
-            />
-          </div>
-          <div className="menuCon">
-            <div className="searchBar">
-              <SearchBar />
-            </div>
-            <div className="rightMenu">
-              <RightMenu />
-            </div>
-            <Button
-              className="barsMenu"
-              type="primary"
-              onClick={this.showDrawer}
-            >
-              <span className="barsBtn" />
-            </Button>
-            <Drawer
-              title="Basic Drawer"
-              placement="right"
-              closable={false}
-              onClose={this.onClose}
-              visible={this.state.visible}
-            >
-              <RightMenuDrawer />
-            </Drawer>
-          </div>
-        </nav>
-        <div className="searchBarMobile">
-          <SearchBar />
+      <nav className="menuBar">
+        <div className="logo">
+          <img
+            alt="logo"
+            src={process.env.PUBLIC_URL + '/images/br_logo_xs.jpg'}
+          />
         </div>
-      </Fragment>
+        <div className="menuCon">
+          <div className="rightMenu">
+            <RightMenu />
+          </div>
+          <Button className="barsMenu" type="primary" onClick={this.showDrawer}>
+            <span className="barsBtn" />
+          </Button>
+          <Drawer
+            title="Basic Drawer"
+            placement="right"
+            closable={false}
+            onClose={this.onClose}
+            visible={this.state.visible}
+          >
+            <RightMenuDrawer />
+          </Drawer>
+        </div>
+      </nav>
     );
   }
 }
