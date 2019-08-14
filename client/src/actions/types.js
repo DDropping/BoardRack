770c051b4e378f59827762f61401f156f543965c
David Dropping <ddropping@gmail.com>
2019-08-14 12:01:10 -0700

    Add drawer types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,5 @@
-export const SET_ALERT = 'SET_ALERT'
-export const REMOVE_ALERT = 'REMOVE_ALERT'
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
+
+export const OPEN_DRAWER = 'OPEN_DRAWER';
+export const CLOSE_DRAWER = 'CLOSE_DRAWER';
