184c703d62d31a4d6c32e380cb16df5722e4f880
David Dropping <ddropping@gmail.com>
2019-08-16 14:18:42 -0700

    Add onSubmit to login and register forms

--- client/src/components/navbar/login/LoginForm.js
+++ client/src/components/navbar/login/LoginForm.js
@@ -1,57 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { Link } from 'react-router-dom';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import { reduxForm, Field } from 'redux-form';
-import { SelectField, TextField } from 'redux-form-antd';
+import { TextField } from 'redux-form-antd';
 
 import {
   toggleLoginModal,
   changeToRegisterModal
 } from '../../../actions/loginModal';
 
-const LoginForm = props => {
-  return (
-    <Form>
-      {/* FIX: force antd to load input style */}
-      <Input style={{ display: 'none' }} />
-      <Field
-        name="email"
-        component={TextField}
-        prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
-        placeholder="Email"
-        size="large"
-      />
-      <Field
-        name="password"
-        component={TextField}
-        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-        placeholder="Email"
-        size="large"
-      />
-      <Checkbox>Remember me</Checkbox>
-      <Link to="" className="login-form-forgot" style={{ float: 'right' }}>
-        Forgot password
-      </Link>
-      <Button
-        type="primary"
-        size="large"
-        htmlType="submit"
-        className="login-form-button"
-        style={{ width: '100%' }}
-      >
-        Log in
-      </Button>
-      <div style={{ paddingTop: '20px' }}>
-        Don't have and account yet?{' '}
-        <Link to="" onClick={props.changeToRegisterModal}>
-          register now!
+class LoginForm extends Component {
+  onSubmit = formProps => {
+    console.log(formProps);
+  };
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <Form onSubmit={handleSubmit(this.onSubmit)}>
+        {/* FIX: force antd to load input style */}
+        <Input style={{ display: 'none' }} />
+        <Field
+          name="email"
+          component={TextField}
+          prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          placeholder="Email"
+          size="large"
+        />
+        <Field
+          name="password"
+          component={TextField}
+          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          placeholder="Email"
+          size="large"
+        />
+        <Checkbox>Remember me</Checkbox>
+        <Link to="" className="login-form-forgot" style={{ float: 'right' }}>
+          Forgot password
         </Link>
-      </div>
-    </Form>
-  );
-};
+        <Button
+          type="primary"
+          size="large"
+          htmlType="submit"
+          className="login-form-button"
+          style={{ width: '100%' }}
+        >
+          Log in
+        </Button>
+        <div style={{ paddingTop: '20px' }}>
+          Don't have and account yet?{' '}
+          <Link to="" onClick={this.props.changeToRegisterModal}>
+            register now!
+          </Link>
+        </div>
+      </Form>
+    );
+  }
+}
 
 export default compose(
   connect(
--- client/src/components/navbar/register/RegisterForm.js
+++ client/src/components/navbar/register/RegisterForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { Link } from 'react-router-dom';
@@ -7,66 +7,73 @@ import { reduxForm, Field } from 'redux-form';
 import { TextField } from 'redux-form-antd';
 import { changeToLoginModal } from '../../../actions/registerModal';
 
-const RegisterForm = props => {
-  return (
-    <Form>
-      {/* FIX: force antd to load input style */}
-      <Input style={{ display: 'none' }} />
-      <Field
-        name="email"
-        component={TextField}
-        prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
-        placeholder="Email"
-        size="large"
-        required
-      />
-      <Field
-        name="username"
-        component={TextField}
-        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-        placeholder="Username"
-        size="large"
-        required
-      />
-      <Field
-        name="password"
-        type="password"
-        component={TextField}
-        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-        placeholder="Password"
-        size="large"
-        required
-      />
-      <Field
-        name="confirmPassword"
-        type="text"
-        component={TextField}
-        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-        placeholder="Confirm Password"
-        size="large"
-        required
-      />
-      <small style={{ color: 'rgba(0,0,0,.50)' }}>
-        By clicking Register, you agree to our Terms and Service.
-      </small>
-      <Button
-        type="primary"
-        size="large"
-        htmlType="submit"
-        className="login-form-button"
-        style={{ width: '100%' }}
-      >
-        Register
-      </Button>
-      <div style={{ marginTop: '20px' }}>
-        Already have an account?{' '}
-        <Link to="/" onClick={props.changeToLoginModal}>
-          Log In
-        </Link>
-      </div>
-    </Form>
-  );
-};
+class RegisterForm extends Component {
+  onSubmit = formProps => {
+    console.log(formProps);
+  };
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <Form onSubmit={handleSubmit(this.onSubmit)}>
+        {/* FIX: force antd to load input style */}
+        <Input style={{ display: 'none' }} />
+        <Field
+          name="email"
+          component={TextField}
+          prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          placeholder="Email"
+          size="large"
+          required
+        />
+        <Field
+          name="username"
+          component={TextField}
+          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          placeholder="Username"
+          size="large"
+          required
+        />
+        <Field
+          name="password"
+          type="password"
+          component={TextField}
+          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          placeholder="Password"
+          size="large"
+          required
+        />
+        <Field
+          name="confirmPassword"
+          type="text"
+          component={TextField}
+          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          placeholder="Confirm Password"
+          size="large"
+          required
+        />
+        <small style={{ color: 'rgba(0,0,0,.50)' }}>
+          By clicking Register, you agree to our Terms and Service.
+        </small>
+        <Button
+          type="primary"
+          size="large"
+          htmlType="submit"
+          className="login-form-button"
+          style={{ width: '100%' }}
+        >
+          Register
+        </Button>
+        <div style={{ marginTop: '20px' }}>
+          Already have an account?{' '}
+          <Link to="/" onClick={this.props.changeToLoginModal}>
+            Log In
+          </Link>
+        </div>
+      </Form>
+    );
+  }
+}
 
 export default compose(
   connect(
