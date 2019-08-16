571196efb04f5ac56a508b2b9fe31e941f7f8b63
David Dropping <ddropping@gmail.com>
2019-08-16 12:16:24 -0700

    Add redux form to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import alert from './alert';
 import registerModal from './modals/registerModal';
 import loginModal from './modals/loginModal';
 
 export default combineReducers({
+  formReducer,
   alert,
   registerModal,
   loginModal
