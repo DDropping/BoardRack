bbe45ca6c42ce19bcda9d01268db002d4fc1f502
David Dropping <ddropping@gmail.com>
2019-08-16 12:47:31 -0700

    Update login form to use redux-form-antd

--- client/src/components/navbar/register/RegisterForm.js
+++ client/src/components/navbar/register/RegisterForm.js
@@ -1,62 +1,69 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { compose } from 'redux';
 import { Link } from 'react-router-dom';
 import { Form, Icon, Input, Button } from 'antd';
+import { reduxForm, Field } from 'redux-form';
+import { SelectField, TextField } from 'redux-form-antd';
 import { changeToLoginModal } from '../../../actions/registerModal';
 
 const RegisterForm = props => {
   return (
-    <Form>
-      <Form.Item>
-        <Input
-          size="large"
+    <form>
+      <fieldset>
+        <Field
+          name="email"
+          component={TextField}
           prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          size="large"
           placeholder="Email"
         />
-      </Form.Item>
-      <Form.Item>
-        <Input
-          size="large"
-          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          type="password"
-          placeholder="Username"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Input
-          size="large"
-          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Input
-          size="large"
-          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          type="password"
-          placeholder="Confirm Password"
-        />
-      </Form.Item>
-      <Form.Item>
-        <small>By clicking Register, you agree to our Terms and Service.</small>
-        <Button
-          type="primary"
-          size="large"
-          htmlType="submit"
-          className="login-form-button"
-          style={{ width: '100%' }}
-        >
-          Register
-        </Button>
-        Already have an account?{' '}
+      </fieldset>
+      <Field
+        name="username"
+        component={TextField}
+        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        size="large"
+        placeholder="Username"
+      />
+      <Field
+        name="password"
+        component={TextField}
+        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        size="large"
+        type="password"
+        placeholder="Password"
+      />
+      <Field
+        name="confirmPassword"
+        component={TextField}
+        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        size="large"
+        type="password"
+        placeholder="Confirm Password"
+      />
+      <small>By clicking Register, you agree to our Terms and Service.</small>
+      <Button
+        type="primary"
+        size="large"
+        htmlType="submit"
+        className="login-form-button"
+        style={{ width: '100%' }}
+      >
+        Register
+      </Button>
+      <div style={{ paddingTop: '20px' }}>
+        Already have an account?
         <Link onClick={props.changeToLoginModal}>Log In</Link>
-      </Form.Item>
-    </Form>
+      </div>
+    </form>
   );
 };
 
-export default connect(
-  null,
-  { changeToLoginModal }
+export default compose(
+  connect(
+    null,
+    { changeToLoginModal }
+  ),
+  reduxForm({ form: 'register' })
 )(RegisterForm);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import auth from './auth';
 import alert from './alert';
 import registerModal from './modals/registerModal';
 import loginModal from './modals/loginModal';
 
 export default combineReducers({
-  formReducer,
+  form: formReducer,
+  auth,
   alert,
   registerModal,
   loginModal
