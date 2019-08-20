7da94dfbec6953a38d1deb3f3cc4726b5cd239a7
David Dropping <ddropping@gmail.com>
2019-08-19 18:20:03 -0700

    Update navDrawer with all navigation items

--- client/src/components/navbar/NavItemsDrawer.js
+++ client/src/components/navbar/NavItemsDrawer.js
@@ -1,22 +1,78 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Drawer } from 'antd';
+import { Menu, Icon, Drawer } from 'antd';
 
 import { toggleNavDrawer } from '../../actions/drawer_nav';
+import { toggleRegisterModal } from '../../actions/registerModal';
+import { toggleLoginModal } from '../../actions/loginModal';
+import RegisterModal from './register/RegisterModal';
+import LoginModal from './login/LoginModal';
 
 const NavItemsDrawer = props => {
   return (
     <div>
       <Drawer
-        title="Basic Drawer"
         placement="right"
         closable={false}
         onClose={props.toggleNavDrawer}
         visible={props.isVisible}
+        style={{ padding: '0px !important' }}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <Menu>
+          <Menu.Item key="home">
+            <Icon type="home" style={{ color: 'rgba(0,0,0,.25)' }} />
+            Home
+          </Menu.Item>
+          {props.isAuthenticated && (
+            <Menu.Item key="createPost">
+              <Icon type="plus-circle" style={{ color: 'rgba(0,0,0,.25)' }} />
+              Create Post
+            </Menu.Item>
+          )}
+          {props.isAuthenticated && (
+            <Menu.Item key="myAccount">
+              <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
+              My Account
+            </Menu.Item>
+          )}
+          {props.isAuthenticated && (
+            <Menu.Item key="myPosts">
+              <Icon type="form" style={{ color: 'rgba(0,0,0,.25)' }} />
+              My Posts
+            </Menu.Item>
+          )}
+          {props.isAuthenticated && (
+            <Menu.Item key="myMessages">
+              <Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />
+              My Messages
+            </Menu.Item>
+          )}
+          {props.isAuthenticated && (
+            <Menu.Item key="myFavorites">
+              <Icon type="star" style={{ color: 'rgba(0,0,0,.25)' }} />
+              My Favorites
+            </Menu.Item>
+          )}
+          {props.isAuthenticated && (
+            <Menu.Item key="logout">
+              <Icon type="logout" style={{ color: 'rgba(0,0,0,.25)' }} />
+              Logout
+            </Menu.Item>
+          )}
+
+          {!props.isAuthenticated && (
+            <Menu.Item onClick={props.toggleLoginModal} key="login">
+              <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
+              Login
+            </Menu.Item>
+          )}
+          {!props.isAuthenticated && (
+            <Menu.Item onClick={props.toggleRegisterModal} key="register">
+              <Icon type="user-add" style={{ color: 'rgba(0,0,0,.25)' }} />
+              Register
+            </Menu.Item>
+          )}
+        </Menu>
       </Drawer>
     </div>
   );
@@ -24,11 +80,12 @@ const NavItemsDrawer = props => {
 
 const mapStateToProps = state => {
   return {
-    isVisible: state.navDrawer.isVisible
+    isVisible: state.navDrawer.isVisible,
+    isAuthenticated: state.auth.isAuthenticated
   };
 };
 
 export default connect(
   mapStateToProps,
-  { toggleNavDrawer }
+  { toggleRegisterModal, toggleLoginModal, toggleNavDrawer }
 )(NavItemsDrawer);
