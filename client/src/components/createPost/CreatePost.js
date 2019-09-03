4b07d04816771b46d24cc8425788ba967636ae3c
David Dropping <ddropping@gmail.com>
2019-09-03 15:05:15 -0700

    Create createpost.css

--- client/src/components/createPost/CreatePost.js
+++ client/src/components/createPost/CreatePost.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './createPost.css';
 
 const CreatePost = () => {
   return (
-    <div>
+    <div className="wrapper">
       <div>Create Post Page</div>
     </div>
   );
