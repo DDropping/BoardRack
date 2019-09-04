61b70c6b07b2f4a5c11492f74b37d3b7eb18a603
David Dropping <ddropping@gmail.com>
2019-09-04 13:15:44 -0700

    Update AddDetails to have row/col between details and photos

--- client/src/components/createPost/AddDetails.js
+++ client/src/components/createPost/AddDetails.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Form, Icon, Button, Row, Col, Select, Divider, Input } from 'antd';
+
+import AddDetailsInfo from './AddDetailsInfo';
+import AddDetailsPhotos from './AddDetailsPhotos';
 
 const AddDetails = () => {
   return (
-    <div>
-      <div>Add Details</div>
-    </div>
+    <Row>
+      <Col xs={24} sm={12}>
+        <AddDetailsInfo />
+      </Col>
+      <Col xs={24} sm={12}>
+        Photos
+      </Col>
+    </Row>
   );
 };
 
-export default AddDetails;
+export default connect(
+  null,
+  null
+)(AddDetails);
