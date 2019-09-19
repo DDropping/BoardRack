c52a6388e8b0ba084fa4ab4a1584c837bc46ab52
David Dropping <ddropping@gmail.com>
2019-09-18 17:23:16 -0700

    Create a test upload component

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Test = () => {
   return (
     <div>
-      <div>Test</div>
+      <form method="post" enctype="multipart/form-data" action="/upload">
+        <label for="file">Upload a file</label>
+        <input type="file" name="upload" />
+        <input type="submit" class="button" />
+      </form>
     </div>
   );
 };
