826b6ffe9458468789bab46cda5ae9faafcb7f46
David Dropping <ddropping@gmail.com>
2019-09-19 12:30:05 -0700

    Create multerS3 service function

---
+++ services/multerS3.js
@@ -0,0 +1,30 @@
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
+const S3_KEY = config.get('S3_Access_Key_Id');
+const S3_SECRET = config.get('S3_Secret_Acess_Key');
+const S3_REGION = config.get('S3_Region');
+
+aws.config.update({
+  secretAccessKey: S3_KEY,
+  accessKeyId: S3_SECRET,
+  region: S3_REGION
+});
+
+const s3 = new aws.S3();
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'boardrack',
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    }
+  })
+});
+
+module.exports = upload;
