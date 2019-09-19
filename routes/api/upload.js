b9c9e3f59270c0b2380e1b3d508fa7cb68c4f102
David Dropping <ddropping@gmail.com>
2019-09-19 15:03:52 -0700

    Create image type validation

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -10,6 +10,11 @@ const singleUpload = upload.single('image');
 
 router.post('/', function(req, res) {
   singleUpload(req, res, function(err) {
+    if (err) {
+      return res.status(422).send({
+        errors: [{ title: 'Image Upload Error', detail: err.message }]
+      });
+    }
     return res.json({ imageUrl: req.file.location });
   });
 });
--- services/multerS3.js
+++ services/multerS3.js
@@ -15,10 +15,36 @@ aws.config.update({
 
 const s3 = new aws.S3();
 
+const imageFilter2 = (req, file, callback) => {
+  var path = require('path');
+  var ext = path.extname(file.originalname);
+  if (
+    ext !== '.png' &&
+    ext !== '.PNG' &&
+    ext !== '.jpg' &&
+    ext !== '.JPG' &&
+    ext !== '.jpeg' &&
+    ext !== '.JPEG'
+  ) {
+    return callback(new Error('Only images are allowed'));
+  }
+  callback(null, true);
+};
+
+const imageFilter = (req, res, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file type, only JPEG and PNG allowed'), false);
+  }
+};
+
 const upload = multer({
+  fileFilter: imageFilter2,
   storage: multerS3({
     s3: s3,
     bucket: 'boardrack',
+    acl: 'public-read',
     metadata: function(req, file, cb) {
       cb(null, { fieldName: file.fieldname });
     },
