671cc131e33f3adb2c87904f7a446fb0200940b8
David Dropping <ddropping@gmail.com>
2019-09-23 15:11:28 -0700

    Install browser-image-compression, and implent image file compression"

--- client/src/actions/createPost/imageUpload.js
+++ client/src/actions/createPost/imageUpload.js
@@ -1,17 +1,41 @@
 import axios from 'axios';
+import imageCompression from 'browser-image-compression';
 import { ADD_IMG_URL_TO_STORE } from '../types';
 
 export const uploadImage = uploadedImage => async dispatch => {
-  const fd = new FormData();
-  fd.append('image', uploadedImage, uploadedImage.name);
-  const res = await axios.post('api/upload', fd, {
-    onUploadProgress: progressEvent => {
-      console.log(
-        'Upload Progress: ' +
-          Math.round((progressEvent.loaded / progressEvent.total) * 100) +
-          '%'
-      );
-    }
-  });
-  dispatch({ type: ADD_IMG_URL_TO_STORE, payload: res.data.imageUrl });
+  console.log('In upload action...');
+
+  const options = {
+    maxSizeMB: 1,
+    maxWidthOrHeight: 1200,
+    useWebWorker: true
+  };
+
+  const imageFile = uploadedImage;
+  console.log('originalFile instanceof Blob', imageFile instanceof Blob); // true
+  console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
+
+  try {
+    const compressedFile = await imageCompression(imageFile, options);
+    console.log(
+      'compressedFile instanceof Blob',
+      compressedFile instanceof Blob
+    ); // true
+    console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`);
+
+    const fd = new FormData();
+    fd.append('image', compressedFile, compressedFile.name);
+    const res = await axios.post('api/upload', fd, {
+      onUploadProgress: progressEvent => {
+        console.log(
+          'Upload Progress: ' +
+            Math.round((progressEvent.loaded / progressEvent.total) * 100) +
+            '%'
+        );
+      }
+    });
+    dispatch({ type: ADD_IMG_URL_TO_STORE, payload: res.data.imageUrl });
+  } catch (err) {
+    console.log(err);
+  }
 };
