c41a6dd4a432a3cd959d80ee621e25bed0bbaaac
David Dropping <ddropping@gmail.com>
2019-09-23 00:05:49 -0700

    Update image upload component

--- client/src/components/createPost/addDetails/imageUpload/ImageUpload.js
+++ client/src/components/createPost/addDetails/imageUpload/ImageUpload.js
@@ -11,37 +11,24 @@ import { Icon } from 'antd';
 import './imgUpload.css';
 import { uploadImage } from '../../../../actions/createPost/imageUpload';
 
-const fileUploadHandler = event => {
-  const fd = new FormData();
-  fd.append('image', event.target.files[0], event.target.files[0].name);
-  axios
-    .post('api/upload', fd, {
-      onUploadProgress: progressEvent => {
-        console.log(
-          'Upload Progress: ' +
-            Math.round((progressEvent.loaded / progressEvent.total) * 100) +
-            '%'
-        );
-      }
-    })
-    .then(res => {
-      console.log(res);
-    });
-};
-
-const ImageUpload = () => {
-  return (
-    <div className="upload-btn-wrapper">
-      <button className="btn">
-        {/**        <Icon type={this.state.loading ? 'loading' : 'plus'} />
-         */}
-        <Icon type="plus" />
-        <div className="ant-upload-text">Upload</div>
-      </button>
-      <input type="file" onChange={fileUploadHandler} />
-    </div>
-  );
-};
+class ImageUpload extends React.Component {
+  render() {
+    return (
+      <div className="upload-btn-wrapper">
+        <button className="btn">
+          {/**        <Icon type={this.state.loading ? 'loading' : 'plus'} />
+           */}
+          <Icon type="plus" />
+          <div className="ant-upload-text">Upload</div>
+        </button>
+        <input
+          type="file"
+          onChange={event => this.props.uploadImage(event.target.files[0])}
+        />
+      </div>
+    );
+  }
+}
 
 export default connect(
   null,
