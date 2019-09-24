3dc18e6a0a18473a659e2e77652e67c4acb677ef
David Dropping <ddropping@gmail.com>
2019-09-23 17:51:18 -0700

    Create test thumbnail image component

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -1,42 +1,25 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Icon } from 'antd';
+import React from 'react';
+import { Card, Icon } from 'antd';
 
-class Test extends Component {
-  fileUploadHandler = event => {
-    const fd = new FormData();
-    fd.append('image', event.target.files[0], event.target.files[0].name);
-    axios
-      .post('api/upload', fd, {
-        onUploadProgress: progressEvent => {
-          console.log(
-            'Upload Progress: ' +
-              Math.round((progressEvent.loaded / progressEvent.total) * 100) +
-              '%'
-          );
+const Test = () => {
+  return (
+    <div>
+      <Card
+        style={{ width: 240, padding: 'none' }}
+        bodyStyle={{ padding: 0 }}
+        cover={
+          <img
+            alt="example"
+            src="https://boardrack.s3-us-west-1.amazonaws.com/1569281898504"
+          />
         }
-      })
-      .then(res => {
-        console.log(res);
-      });
-  };
-
-  render() {
-    const uploadButton = (
-      <div>
-        <Icon type={this.state.loading ? 'loading' : 'plus'} />
-        <div className="ant-upload-text">Upload</div>
-      </div>
-    );
-    return (
-      <div>
-        <div className="upload-btn-wrapper">
-          <button className="btn">{uploadButton}</button>
-          <input type="file" onChange={this.fileUploadHandler} />
-        </div>
-      </div>
-    );
-  }
-}
+        actions={[
+          <Icon type="star" key="star" />,
+          <Icon type="delete" key="delete" />
+        ]}
+      ></Card>
+    </div>
+  );
+};
 
 export default Test;
