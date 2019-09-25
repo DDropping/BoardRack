fa7efc63d809381297cc38be275b07c5ec1a44f5
David Dropping <ddropping@gmail.com>
2019-09-25 13:06:59 -0700

    Create preview image style sheet

--- client/src/components/createPost/addDetails/imageUpload/PreviewImage.js
+++ client/src/components/createPost/addDetails/imageUpload/PreviewImage.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import { Card, Icon } from 'antd';
 
+import './imgUpload.css';
+
 const PreviewImage = previewObjectUrl => {
   return (
     <Card
-      style={{
-        width: '150px',
-        height: '200px',
-        backgroundColor: '#f3f7f9',
-        marginBottom: '10px',
-        marginRight: '10px',
-        display: 'inline-block',
-        lineHeight: '148px'
-      }}
+      className="preview-img-card"
       hoverable={true}
       bodyStyle={{ padding: 0 }}
       cover={
         <div style={{ width: 150, height: 150, textAlign: 'center' }}>
           <img
-            style={{
-              maxWidth: '148px',
-              maxHeight: '150px',
-              margin: 'auto',
-              position: 'relative',
-              right: '1px' //fix antd-card-border offset by 1 px
-            }}
+            className="preview-img-card-img"
             alt="surfboard"
             src={previewObjectUrl.previewObjectUrl}
           />
