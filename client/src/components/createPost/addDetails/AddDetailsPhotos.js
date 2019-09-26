f90c13541c1c6bda2b09368d4b41e1de1986fb24
David Dropping <ddropping@gmail.com>
2019-09-25 19:35:36 -0700

    Remove unused imports

--- client/src/components/createPost/addDetails/AddDetailsPhotos.js
+++ client/src/components/createPost/addDetails/AddDetailsPhotos.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import ImageUploadButton from './imageUpload/ImageUploadButton';
 import PreviewList from './imageUpload/PreviewList';
 
 const AddDetailsPhotos = () => {
