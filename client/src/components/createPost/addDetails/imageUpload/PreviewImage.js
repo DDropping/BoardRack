c4e4c1d9dcb31adf9719990908dc43d33dc2f15e
David Dropping <ddropping@gmail.com>
2019-09-25 19:14:52 -0700

    Fix previewImange: inline issue

--- client/src/components/createPost/addDetails/imageUpload/PreviewImage.js
+++ client/src/components/createPost/addDetails/imageUpload/PreviewImage.js
@@ -14,49 +14,47 @@ const PreviewImage = props => {
   });
 
   return (
-    <div>
-      <Card
-        style={{
-          width: '150px',
-          height: '200px',
-          backgroundColor: '#f3f7f9',
-          marginBottom: '10px',
-          marginRight: '10px',
-          display: 'inline-block',
-          lineHeight: '150px'
-        }}
-        hoverable={true}
-        bodyStyle={{ padding: 0 }}
-        cover={
-          <div style={{ width: 150, height: 150, textAlign: 'center' }}>
-            {isLoading ? (
-              <div style={{ display: 'inline-block' }}>
-                <img
-                  className="preview-img-card-img-loading"
-                  alt="surfboard"
-                  src={props.previewObjectUrl}
-                />
-                <Icon
-                  className="preview-img-upload-loading-icon"
-                  type="loading"
-                  key="loading"
-                />
-              </div>
-            ) : (
+    <Card
+      style={{
+        width: '150px',
+        height: '200px',
+        backgroundColor: '#f3f7f9',
+        marginBottom: '10px',
+        marginRight: '10px',
+        display: 'inline-block',
+        lineHeight: '150px'
+      }}
+      hoverable={true}
+      bodyStyle={{ padding: 0 }}
+      cover={
+        <div className="preview-img-card-img-parent">
+          {isLoading ? (
+            <div style={{ display: 'inline-block' }}>
               <img
-                className="preview-img-card-img"
+                className="preview-img-card-img-loading"
                 alt="surfboard"
                 src={props.previewObjectUrl}
               />
-            )}
-          </div>
-        }
-        actions={[
-          <Icon type="star" key="star" />,
-          <Icon type="delete" key="delete" onClick={props.deleteImagePreview} />
-        ]}
-      ></Card>
-    </div>
+              <Icon
+                className="preview-img-upload-loading-icon"
+                type="loading"
+                key="loading"
+              />
+            </div>
+          ) : (
+            <img
+              className="preview-img-card-img"
+              alt="surfboard"
+              src={props.previewObjectUrl}
+            />
+          )}
+        </div>
+      }
+      actions={[
+        <Icon type="star" key="star" />,
+        <Icon type="delete" key="delete" onClick={props.deleteImagePreview} />
+      ]}
+    ></Card>
   );
 };
 
