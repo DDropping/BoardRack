84a1a632108fdfe5c750c35aeec668baec541edd
David Dropping <ddropping@gmail.com>
2019-09-28 13:31:17 -0700

    Update create post action

--- client/src/actions/createPost/index.js
+++ client/src/actions/createPost/index.js
@@ -1,5 +1,37 @@
+import axios from 'axios';
+
 export const createPost = (formProps, imgList) => async dispatch => {
-  console.log('Inside createPost action...');
-  console.log(formProps);
-  console.log(imgList);
+  const images = imgList.map(obj => {
+    return {
+      thumbnail: obj.imgThumbnail,
+      default: obj.imgDefault
+    };
+  });
+
+  const postItems = formProps;
+  postItems.images = images;
+
+  //set headers for request
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  //stringify the form items
+  const body = JSON.stringify(postItems);
+
+  //post new account to DB
+  try {
+    const res = await axios.post('/api/posts', body, config);
+  } catch (err) {
+    if (err) {
+      console.log(err);
+    }
+  }
+
+  //   console.log('Inside createPost action...');
+  //   console.log(formProps);
+  //   console.log(images);
+  //   console.log(body);
 };
