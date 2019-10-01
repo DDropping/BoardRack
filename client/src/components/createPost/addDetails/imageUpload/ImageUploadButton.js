d2fc8104b495223d5eb2734ec4097b37194fecd2
David Dropping <ddropping@gmail.com>
2019-10-01 14:10:29 -0700

    Update imageUploadButton to be a functional component

--- client/src/components/createPost/addDetails/imageUpload/ImageUploadButton.js
+++ client/src/components/createPost/addDetails/imageUpload/ImageUploadButton.js
@@ -5,37 +5,33 @@ import { Icon } from 'antd';
 import './imgUpload.css';
 import { uploadImage } from '../../../../actions/createPost/imageUpload';
 
-class ImageUploadButton extends React.Component {
-  handleChange(file) {
-    this.props.uploadImage(this.props.imgKey, file);
-  }
+const ImageUploadButton = props => {
+  const handleChange = file => {
+    props.uploadImage(props.imgKey, file);
+  };
 
-  render() {
-    return (
-      <div
-        className="upload-btn-wrapper"
-        style={{
-          display: 'inline-block',
-          verticalAlign: 'top',
-          marginBottom: '10px',
-          marginRight: '10px'
-        }}
-      >
-        <button className="btn" style={{ width: '150px', height: '200px' }}>
-          <Icon type={this.props.isLoading ? 'loading' : 'plus'} />
+  return (
+    <div
+      className="upload-btn-wrapper"
+      style={{
+        display: 'inline-block',
+        verticalAlign: 'top',
+        marginBottom: '10px',
+        marginRight: '10px'
+      }}
+    >
+      <button className="btn" style={{ width: '150px', height: '200px' }}>
+        <Icon type={props.isLoading ? 'loading' : 'plus'} />
 
-          <div className="ant-upload-text">
-            Upload {this.props.imgList.length}/8
-          </div>
-        </button>
-        <input
-          type="file"
-          onChange={event => this.handleChange(event.target.files[0])}
-        />
-      </div>
-    );
-  }
-}
+        <div className="ant-upload-text">Upload {props.imgList.length}/8</div>
+      </button>
+      <input
+        type="file"
+        onChange={event => handleChange(event.target.files[0])}
+      />
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return {
