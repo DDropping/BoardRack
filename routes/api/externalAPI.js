34fe18349ea22d7315f3a89c78e86fb5bed8112c
David Dropping <ddropping@gmail.com>
2019-10-03 18:34:05 -0700

    Create api route to retrieve user location given geolocation

---
+++ routes/api/externalAPI.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+const config = require('config');
+const HERE_REST_APP_ID = config.get('here_REST_App_Id');
+const HERE_REST_APP_CODE = config.get('here_REST_App_Code');
+
+// @route   GET api/externalAPI/getAddress
+// @desc    Get user address provided lat/lng
+// @access  Public
+router.post('/getAddress', async (req, res) => {
+  const { lat, lng } = req.body;
+  try {
+    const location = await axios.get(
+      `https://reverse.geocoder.api.here.com/6.2/reversegeocode.json?app_id=${HERE_REST_APP_ID}&app_code=${HERE_REST_APP_CODE}&mode=retrieveAddresses&prox=${lat},${lng}`
+    );
+    const Country =
+      location.data.Response.View[0].Result[0].Location.Address.Country;
+    const State =
+      location.data.Response.View[0].Result[0].Location.Address.State;
+    const City = location.data.Response.View[0].Result[0].Location.Address.City;
+    const PostalCode =
+      location.data.Response.View[0].Result[0].Location.Address.PostalCode;
+    const address = JSON.stringify({ Country, State, City, PostalCode });
+    console.log(address);
+
+    res.json(address);
+  } catch {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
