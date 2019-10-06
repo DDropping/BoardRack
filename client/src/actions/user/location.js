9733ca69f535f9318c0587bee691578afba09df8
David Dropping <ddropping@gmail.com>
2019-10-06 10:44:22 -0700

    Create route to save user location

--- client/src/actions/user/location.js
+++ client/src/actions/user/location.js
@@ -20,14 +20,49 @@ export const getUserAddress = ({ lat, lng }) => async dispatch => {
     dispatch({ type: UPDATE_GEOLOCATION, payload: res.data });
 
     if (store.getState().auth.user) {
-      console.log('user logged in');
-      updateUserLocation();
+      console.log('user is logged in');
+      //if user is logged in
+      if (!store.getState().auth.user.location) {
+        //if user doesn't have a saved location yet
+        console.log('Saving current location to account');
+        //updateUserLocation();
+        const body = {
+          location: {
+            lat: res.data.lat,
+            lng: res.data.lng,
+            country: res.data.Country,
+            state: res.data.State,
+            city: res.data.City,
+            zip: res.data.PostalCode
+          }
+        };
+        console.log('body: ' + res.data.lat);
+        const res2 = await axios.put('/api/accounts/updateLocation', body);
+        console.log(res2);
+      }
     }
   } catch (err) {
     console.log(err);
   }
 };
 
-export const updateUserLocation = () => {
+//set user's location
+export const updateUserLocation = location => async dispatch => {
   console.log('inside update user location');
+  console.log(location);
+  try {
+    const body = {
+      location: {
+        lat: { type: String },
+        lng: { type: String },
+        country: { type: String },
+        state: { type: String },
+        city: { type: String },
+        zip: { type: String }
+      }
+    };
+    const res = await axios.put('/api/accounts/updateLocation');
+  } catch (err) {
+    console.log(err);
+  }
 };
