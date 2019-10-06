3b915414e8456bf49c2b806effb7c58628472b4c
David Dropping <ddropping@gmail.com>
2019-10-06 13:07:13 -0700

    Create location reducers

--- client/src/reducers/user/location.js
+++ client/src/reducers/user/location.js
@@ -1,6 +1,12 @@
-import { UPDATE_GEOLOCATION } from '../../actions/types';
+import {
+  UPDATE_GEOLOCATION,
+  USER_LOADED_SET_LOCATION,
+  LOADING_USER_LOCATION,
+  LOADING_USER_LOCATION_DONE
+} from '../../actions/types';
 
 const initialState = {
+  isLoading: false,
   lat: null,
   lng: null,
   country: null,
@@ -11,6 +17,26 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case LOADING_USER_LOCATION:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case LOADING_USER_LOCATION_DONE:
+      return {
+        ...state,
+        isLoading: false
+      };
+    case USER_LOADED_SET_LOCATION:
+      return {
+        ...state,
+        lat: action.payload.lat,
+        lng: action.payload.lng,
+        country: action.payload.country,
+        state: action.payload.state,
+        city: action.payload.city,
+        postalCode: action.payload.zip
+      };
     case UPDATE_GEOLOCATION:
       return {
         ...state,
