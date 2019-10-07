9189ad36b5218daed9e1695483d7e30e7dc8e1c1
David Dropping <ddropping@gmail.com>
2019-10-07 15:50:49 -0700

    Create route to retrieve geolocation given address

--- routes/api/externalAPI.js
+++ routes/api/externalAPI.js
@@ -6,7 +6,7 @@ const config = require('config');
 const HERE_REST_APP_ID = config.get('here_REST_App_Id');
 const HERE_REST_APP_CODE = config.get('here_REST_App_Code');
 
-// @route   GET api/externalAPI/getAddress
+// @route   POST api/externalAPI/getAddress
 // @desc    Get user address provided lat/lng
 // @access  Public
 router.post('/getAddress', async (req, res) => {
@@ -24,9 +24,40 @@ router.post('/getAddress', async (req, res) => {
       PostalCode
     } = location.data.Response.View[0].Result[0].Location.Address;
 
+    const address = { lat, lng, Country, State, City, PostalCode };
+
+    res.json(address);
+  } catch {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   POST api/externalAPI/getCoords
+// @desc    Get user lat/lng given address
+// @access  Public
+router.post('/getCoords', async (req, res) => {
+  const { city, state, postalCode } = req.body;
+  try {
+    const location = await axios.get(
+      `https://geocoder.api.here.com/6.2/geocode.json?app_id=${HERE_REST_APP_ID}&app_code=${HERE_REST_APP_CODE}&searchtext=${city}+${state}+${postalCode}`
+    );
+
+    //setup response
+    const {
+      Latitude,
+      Longitude
+    } = location.data.Response.View[0].Result[0].Location.DisplayPosition;
+    const {
+      Country,
+      State,
+      City,
+      PostalCode
+    } = location.data.Response.View[0].Result[0].Location.Address;
+
     const address = {
-      lat,
-      lng,
+      lat: Latitude,
+      lng: Longitude,
       Country,
       State,
       City,
