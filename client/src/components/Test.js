7e590382dd5a99694c6b11ec19cc72027c17b52f
David Dropping <ddropping@gmail.com>
2019-10-09 22:51:33 -0700

    Create basic post modal

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -2,22 +2,12 @@ import React from 'react';
 
 import './createPost/addDetails/imageUpload/imgUpload.css';
 import { connect } from 'react-redux';
-import { deleteImagePreview } from '../actions/createPost/imageUpload';
-import GetLocation from './createPost/confirmPost/location/GetLocationButton';
-import LocationForm from './createPost/confirmPost/location/LocationForm';
-import LocationText from './createPost/confirmPost/location/LocationText';
+import PostModal from './post/PostModal';
 
 const Test = props => {
   return (
     <div>
-      {!props.displayLocationForm ? (
-        <div>
-          <GetLocation />
-          <LocationText />
-        </div>
-      ) : (
-        <LocationForm />
-      )}
+      <PostModal />
     </div>
   );
 };
@@ -30,5 +20,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  { deleteImagePreview }
+  null
 )(Test);
---
+++ client/src/components/post/PostModal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PostModal = () => {
+  return (
+    <div className="br-post-modal-grey">
+      <div className="br-post-modal-wrapper">
+        loremksdjf ksdf jsdjf klsjd flkjda kfjkdsj fadflk jsd f sdfljdlkjfksdfk
+        f jdf jdkfjlksf dfj kldf fj sdfjsald jfdf; asdf;s
+      </div>
+    </div>
+  );
+};
+
+export default PostModal;
