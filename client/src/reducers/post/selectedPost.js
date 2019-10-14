0330ebc9761a8f97510f5f712a8d185f81e3a7ae
David Dropping <ddropping@gmail.com>
2019-10-13 23:38:45 -0700

    Update selectedPost

--- client/src/reducers/post/selectedPost.js
+++ client/src/reducers/post/selectedPost.js
@@ -1,4 +1,4 @@
-import { OPEN_POST_MODAL, CLOSE_POST_MODAL } from '../../actions/types';
+//import { OPEN_POST_MODAL, CLOSE_POST_MODAL } from '../../actions/types';
 
 const initialState = {
   _id: null,
@@ -34,17 +34,6 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    case OPEN_POST_MODAL:
-      console.log('post modal reducer');
-      return {
-        ...state,
-        isVisible: true
-      };
-    case CLOSE_POST_MODAL:
-      return {
-        ...state,
-        isVisible: false
-      };
     default:
       return state;
   }
