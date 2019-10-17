9ec488307eb3cde4dba3dd8be31f1f3f73e373fa
David Dropping <ddropping@gmail.com>
2019-10-16 21:20:25 -0700

    Update zIndex for post modal

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -3,14 +3,12 @@ import React from 'react';
 import './createPost/addDetails/imageUpload/imgUpload.css';
 import { OPEN_POST_MODAL } from '../actions/types';
 import { connect, useDispatch } from 'react-redux';
-import PostModal from './post/postModal/PostModal';
 import PostPreview from './post/postPreview/PostPreview';
 
 const Test = props => {
   const dispatch = useDispatch();
   return (
     <div>
-      <PostModal />
       <button onClick={() => dispatch({ type: OPEN_POST_MODAL })}>Open</button>
       <PostPreview />
     </div>
