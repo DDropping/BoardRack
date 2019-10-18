f56b6ca297809c62beaa32e236fdd33c2f89cf99
David Dropping <ddropping@gmail.com>
2019-10-17 19:50:34 -0700

    Update post prievew card with post data

--- client/src/components/post/postPreview/PostPreview.js
+++ client/src/components/post/postPreview/PostPreview.js
@@ -6,21 +6,20 @@ import '../post.css';
 import { OPEN_POST_MODAL } from '../../../actions/types';
 const { Meta } = Card;
 
-const PostPreview = () => {
+const PostPreview = post => {
   const dispatch = useDispatch();
+  console.log(post.post);
   return (
     <div className="br-post-preview-card">
       <Card
         onClick={() => dispatch({ type: OPEN_POST_MODAL })}
         hoverable
-        cover={
-          <img
-            alt="example"
-            src="https://cdn10.bigcommerce.com/s-baaesh4/products/240/images/1198/sd-deck__12993.1516652995.1280.1280__38990.1523919633.1280.1280.jpg?c=2"
-          />
-        }
+        cover={<img alt="example" src={post.post.images[0].thumbnail} />}
       >
-        <Meta title="Rusty SD" description="6'2 x 19 1/4 x 2 3/8" />
+        <Meta
+          title={'$' + post.post.price + ' ' + post.post.title}
+          description="6'2 x 19 1/4 x 2 3/8"
+        />
       </Card>
     </div>
   );
