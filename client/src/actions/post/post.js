012ab39af7a74a9585b5d2f9d7d154690ded39ed
David Dropping <ddropping@gmail.com>
2019-10-17 17:08:33 -0700

    Update load posts action

--- client/src/actions/post/post.js
+++ client/src/actions/post/post.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 import { LOAD_POSTS } from '../types';
 
 export const loadPosts = () => async dispatch => {
-  const res = await axios.get('/api/posts');
-  dispatch({ type: LOAD_POSTS, payload: res.data });
-  console.log(res.data);
+  try {
+    const res = await axios.get('/api/posts');
+    dispatch({ type: LOAD_POSTS, payload: res.data });
+  } catch (err) {
+    console.log(err);
+  }
 };
