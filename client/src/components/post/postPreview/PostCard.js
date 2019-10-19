6a366cb491c9decc424f1008cf60485ea8f86d25
David Dropping <ddropping@gmail.com>
2019-10-19 14:25:54 -0700

    Create basic post card content

--- client/src/components/post/postPreview/PostCard.js
+++ client/src/components/post/postPreview/PostCard.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import '../post.css';
 import PostCardHeader from './PostCardHeader';
+import PostCardContent from './PostCardContent';
 
 const PostCard = () => {
   return (
     <div className="br-post-card-container">
       <PostCardHeader />
       <div className="br-post-card-img">Post img</div>
-      <div className="br-post-card-content">Post Card content</div>
+      <PostCardContent />
     </div>
   );
 };
---
+++ client/src/components/post/postPreview/PostCardContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../post.css';
+
+const PostCardContent = () => {
+  return (
+    <div>
+      <div className="br-post-card-content">Post Card content</div>
+    </div>
+  );
+};
+
+export default PostCardContent;
