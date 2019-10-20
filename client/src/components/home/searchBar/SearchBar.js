7727dc5bcf316c74f6dcf50937f2cdd4858c0b99
David Dropping <ddropping@gmail.com>
2019-10-20 16:33:12 -0700

    update searchbar css

--- client/src/components/home/searchBar/SearchBar.js
+++ client/src/components/home/searchBar/SearchBar.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './searchBar.css';
 
 const SearchBar = () => {
-  return <div className="br-search-bar-container ">Search Bar</div>;
+  return (
+    <div className="br-search-bar-wrapper">
+      <div className="br-search-bar-container ">Search Bar</div>
+    </div>
+  );
 };
 
 export default SearchBar;
