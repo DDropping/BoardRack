83501a22db0d03973bd758fa0cf606616003916b
David Dropping <ddropping@gmail.com>
2019-10-23 22:55:40 -0700

    Update postCard component to check if user has favorited the post

--- client/src/components/post/postPreview/PostCard.js
+++ client/src/components/post/postPreview/PostCard.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 import '../post.css';
 import PostCardHeader from './PostCardHeader';
@@ -6,9 +7,25 @@ import PostCardImg from './PostCardImg';
 import PostCardContent from './PostCardContent';
 
 const PostCard = post => {
+  const user = useSelector(state => state.auth.user);
+  const isFavorite = false;
+
+  useEffect(() => checkFavorite(user), []);
+
+  const checkFavorite = user => {
+    if (user !== null) {
+      if (
+        post.post.favorites.filter(
+          favorite => favorite.user.toString() === user._id
+        ).length > 0
+      ) {
+        isFavorite = true;
+      }
+    }
+  };
   return (
     <div className="br-post-card-container">
-      <PostCardHeader post={post.post} />
+      <PostCardHeader post={post.post} isFavoriteInitalValue={isFavorite} />
       <PostCardImg post={post.post} />
       <PostCardContent post={post.post} />
     </div>
