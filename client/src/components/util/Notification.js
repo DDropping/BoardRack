7fce30df9a8a58528801dde13264ec3ab3c7cac9
David Dropping <ddropping@gmail.com>
2019-10-24 18:07:30 -0700

    Delete repitious code: notification blocks

--- client/src/components/util/Notification.js
+++ client/src/components/util/Notification.js
@@ -28,58 +28,6 @@ export const failNotification = (message, description, duration) => {
   });
 };
 
-export const LoginSuccessNotification = () => {
-  notification.open({
-    top: 65,
-    message: 'Login Successful!',
-    description: 'You Are Now Logged In',
-    duration: 3,
-    icon: <Icon type="check-circle" theme="twoTone" twoToneColor="#52c41a" />
-  });
-};
-
-export const RegisterSuccessNotification = () => {
-  notification.open({
-    top: 65,
-    message: 'Registration Successful!',
-    description: 'You Are Now Logged In',
-    duration: 3,
-    icon: <Icon type="check-circle" theme="twoTone" twoToneColor="#52c41a" />
-  });
-};
-
-export const LogoutSuccessNotification = () => {
-  notification.open({
-    top: 65,
-    message: 'Logout Successful!',
-    description: 'You Are Now Logged Out',
-    duration: 3,
-    icon: <Icon type="check-circle" theme="twoTone" twoToneColor="#52c41a" />
-  });
-};
-
-export const CreatePostSuccessNotification = () => {
-  notification.open({
-    top: 65,
-    message: 'New Post Created!',
-    description: 'Your Post Has Been Created And Is Live For The World To See',
-    duration: 4.5,
-    icon: <Icon type="check-circle" theme="twoTone" twoToneColor="#52c41a" />
-  });
-};
-
-export const CreatePostFailNotification = () => {
-  notification.open({
-    top: 65,
-    message: 'Uhh Ohh, Something Went Wrong',
-    description: 'Sorry, Your Post Could Not Be Created At This Time',
-    duration: 4.5,
-    icon: (
-      <Icon type="exclamation-circle" theme="twoTone" twoToneColor="#ffdc17" />
-    )
-  });
-};
-
 export const UpdateDefaultLocationNotification = body => {
   const key = `open${Date.now()}`;
   const btn = (
