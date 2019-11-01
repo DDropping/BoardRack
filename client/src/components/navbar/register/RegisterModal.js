1c6a45533f35456e807f6fe671901ec8def8f232
David Dropping <ddropping@gmail.com>
2019-10-31 19:02:52 -0700

    Fix registerModal close issue

--- client/src/components/navbar/register/RegisterModal.js
+++ client/src/components/navbar/register/RegisterModal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Modal } from 'antd';
-import { toggleRegisterModal } from '../../../actions/overlay';
+import { TOGGLE_REGISTER_MODAL } from '../../../actions/types';
 import RegisterForm from './RegisterForm';
 
 const Register = () => {
@@ -12,8 +12,8 @@ const Register = () => {
       <Modal
         title="Register"
         visible={isVisible}
-        onOk={dispatch(() => toggleRegisterModal())}
-        onCancel={dispatch(() => toggleRegisterModal())}
+        onOk={() => dispatch({ type: TOGGLE_REGISTER_MODAL })}
+        onCancel={() => dispatch({ type: TOGGLE_REGISTER_MODAL })}
         footer={null}
         zIndex={1100}
       >
