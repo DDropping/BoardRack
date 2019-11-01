964eda6ffce48c866d5e298b31f9e9837801b4aa
David Dropping <ddropping@gmail.com>
2019-10-31 17:27:22 -0700

    Refactor RegisterForm from class to functional component

--- client/src/components/navbar/register/RegisterForm.js
+++ client/src/components/navbar/register/RegisterForm.js
@@ -9,76 +9,74 @@ import { changeToLoginModal } from '../../../actions/overlay';
 import { registerUser } from '../../../actions/auth';
 import { AInput } from '../../formAntComponents';
 
-class RegisterForm extends Component {
-  onSubmit = formProps => {
-    this.props.registerUser(formProps);
+const RegisterForm = props => {
+  const onSubmit = formProps => {
+    props.registerUser(formProps);
   };
 
-  render() {
-    const { handleSubmit } = this.props;
-    let errors = null;
-    if (this.props.registrationErrors) {
-      errors = this.props.registrationErrors.map(error => {
-        return <li key={error}>{error}</li>;
-      });
-    }
-    return (
-      <Form onSubmit={handleSubmit(this.onSubmit)}>
-        {/* FIX: force antd to load input style */}
-        <Field
-          name="email"
-          component={AInput}
-          prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          placeholder="Email"
-          size="large"
-        />
-        <Field
-          name="username"
-          component={AInput}
-          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          placeholder="Username"
-          size="large"
-        />
-        <Field
-          name="password"
-          type="password"
-          component={AInput}
-          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          placeholder="Password"
-          size="large"
-        />
-        <Field
-          name="confirmPassword"
-          type="password"
-          component={AInput}
-          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          placeholder="Confirm Password"
-          size="large"
-        />
-        <ul style={{ color: 'red' }}>{errors}</ul>
-        <small style={{ color: 'rgba(0,0,0,.50)' }}>
-          By clicking Register, you agree to our Terms and Service.
-        </small>
-        <Button
-          type="primary"
-          loading={this.props.isLoading}
-          size="large"
-          htmlType="submit"
-          className="login-form-button"
-          style={{ width: '100%' }}
-        >
-          Register
-        </Button>
-        <div style={{ marginTop: '20px' }}>
-          Already have an account?{' '}
-          <Link to="/" onClick={this.props.changeToLoginModal}>
-            Log In
-          </Link>
-        </div>
-      </Form>
-    );
+  let errors = null;
+  if (props.registrationErrors) {
+    errors = props.registrationErrors.map(error => {
+      return <li key={error}>{error}</li>;
+    });
   }
-}
+
+  return (
+    <Form onSubmit={props.handleSubmit(onSubmit)}>
+      {/* FIX: force antd to load input style */}
+      <Field
+        name="email"
+        component={AInput}
+        prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        placeholder="Email"
+        size="large"
+      />
+      <Field
+        name="username"
+        component={AInput}
+        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        placeholder="Username"
+        size="large"
+      />
+      <Field
+        name="password"
+        type="password"
+        component={AInput}
+        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        placeholder="Password"
+        size="large"
+      />
+      <Field
+        name="confirmPassword"
+        type="password"
+        component={AInput}
+        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        placeholder="Confirm Password"
+        size="large"
+      />
+      <ul style={{ color: 'red' }}>{errors}</ul>
+      <small style={{ color: 'rgba(0,0,0,.50)' }}>
+        By clicking Register, you agree to our Terms and Service.
+      </small>
+      <Button
+        type="primary"
+        loading={props.isLoading}
+        size="large"
+        htmlType="submit"
+        className="login-form-button"
+        style={{ width: '100%' }}
+      >
+        Register
+      </Button>
+      <div style={{ marginTop: '20px' }}>
+        Already have an account?{' '}
+        <Link to="/" onClick={props.changeToLoginModal}>
+          Log In
+        </Link>
+      </div>
+    </Form>
+  );
+};
 
 const mapStateToProps = state => {
   return {
