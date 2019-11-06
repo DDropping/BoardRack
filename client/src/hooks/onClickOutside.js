4eb214d0d3871794a73a0dc5bc1250cbd1dc414c
David Dropping <ddropping@gmail.com>
2019-11-06 15:21:41 -0800

    Create hook to call function when element is clicked outside of ref - useOnClickOutside

---
+++ client/src/hooks/onClickOutside.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+
+// Given a handler function, apply when mouse click outside of given ref
+// Call hook like: useOnClickOutside(ref, useCallback(() => setIsVisible(false)));
+export const useOnClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = event => {
+      // Do nothing if clicking ref's element or descendent elements
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+};
