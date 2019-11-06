4e426aff47384084a23938e6072438cc8dd7281f
David Dropping <ddropping@gmail.com>
2019-11-06 15:45:27 -0800

    Create search input component

---
+++ client/src/components/home/searchBar/SearchInput.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Input } from 'antd';
+
+import './searchInput.css';
+
+const SearchInput = () => {
+  const { Search } = Input;
+
+  return (
+    <div className="br-search-input">
+      <Search
+        placeholder="input search text"
+        enterButton="Search"
+        size="large"
+        onSearch={value => console.log(value)}
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
