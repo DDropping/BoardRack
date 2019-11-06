792ae07e64c03ccf1b9ea5b92cef859deb1debc5
David Dropping <ddropping@gmail.com>
2019-11-06 15:22:32 -0800

    Update LayoutButton component

--- client/src/components/home/searchBar/LayoutButton.js
+++ client/src/components/home/searchBar/LayoutButton.js
@@ -1,16 +1,59 @@
-import React from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import { Icon, Button } from 'antd';
 
+import './layoutButton.css';
+import { useOnClickOutside } from '../../../hooks/onClickOutside';
+
 const LayoutButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [layoutValuem, setLayoutValue] = useState('View');
+  const ref = useRef();
+  useOnClickOutside(ref, useCallback(() => setIsVisible(false)));
+
   return (
-    <Button
-      onClick={() => console.log('hello')}
-      style={{ float: 'right' }}
-      type="link"
-    >
-      <Icon type="appstore" />
-      View
-    </Button>
+    <div className="br-layout-button" ref={ref}>
+      <Button
+        onClick={() => setIsVisible(!isVisible)}
+        style={{ marginRight: '24px' }}
+        type="link"
+      >
+        <Icon type="appstore" />
+        View
+      </Button>
+      {isVisible && (
+        <div className="br-layout-options">
+          <Button
+            onClick={() => {
+              setLayoutValue('List');
+              setIsVisible(false);
+            }}
+            type="link"
+          >
+            <Icon type="menu" /> List
+          </Button>
+          <br />
+          <Button
+            onClick={() => {
+              setLayoutValue('Thumb');
+              setIsVisible(false);
+            }}
+            type="link"
+          >
+            <Icon type="unordered-list" /> Thumb
+          </Button>
+          <br />
+          <Button
+            onClick={() => {
+              setLayoutValue('Gallery');
+              setIsVisible(false);
+            }}
+            type="link"
+          >
+            <Icon type="appstore" /> Gallery
+          </Button>
+        </div>
+      )}
+    </div>
   );
 };
 
