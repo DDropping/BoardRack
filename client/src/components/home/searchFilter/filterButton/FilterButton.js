22e1d7ceb3c8bd82f0b5ad0f72fed287b59ceb02
David Dropping <ddropping@gmail.com>
2019-11-06 15:51:24 -0800

    Update searchBar positioning

--- client/src/components/home/searchFilter/filterButton/FilterButton.js
+++ client/src/components/home/searchFilter/filterButton/FilterButton.js
@@ -3,7 +3,11 @@ import { Icon, Button } from 'antd';
 
 const Filter = () => {
   return (
-    <Button onClick={() => console.log('hello')} type="link">
+    <Button
+      onClick={() => console.log('hello')}
+      style={{ marginLeft: '20px' }}
+      type="link"
+    >
       <Icon type="filter" />
       Filter
     </Button>
