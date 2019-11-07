3fc11b6ad50bde74b28a3344375a64c2e04b7558
David Dropping <ddropping@gmail.com>
2019-11-06 20:16:19 -0800

    Fix remove double div

--- client/src/components/post/postPreview/PostCardImg.js
+++ client/src/components/post/postPreview/PostCardImg.js
@@ -5,13 +5,11 @@ import '../post.css';
 const PostCardImg = post => {
   return (
     <div className="br-post-card-img-wrapper">
-      <div className="br-post-card-img">
-        <img
-          className="br-post-card-img"
-          alt=""
-          src={post.post.images[0].thumbnail}
-        />
-      </div>
+      <img
+        className="br-post-card-img"
+        alt=""
+        src={post.post.images[0].thumbnail}
+      />
     </div>
   );
 };
