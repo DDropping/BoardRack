8dacbc06af8450593b9b1be27249b8b877ee0152
David Dropping <ddropping@gmail.com>
2019-11-07 16:37:05 -0800

    Update searchbar css

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-//import { useSelector, useDispatch } from 'react-redux';
+
+import Filter from './home/searchFilter/filterBox/FilterBox';
 
 const Test = props => {
-  // const dispatch = useDispatch();
-  // const posts = useSelector(state => state.post.postList);
-
   return (
     <div>
-      <div>Test Page</div>
-      <Link to="/">
-        <div style={{ width: '60px' }}>
-          <img
-            style={{ maxHeight: '100%', maxWidth: '100%' }}
-            alt="br-logo-desktop"
-            src={process.env.PUBLIC_URL + '/images/br_logo_small.png'}
-          />
-        </div>
-      </Link>
+      <Filter />
     </div>
   );
 };
