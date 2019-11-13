a95198dada0aa92583e5176646f1c34e5883f20b
David Dropping <ddropping@gmail.com>
2019-11-12 18:52:23 -0800

    Update distance filter with geolocation functionality

--- client/src/components/home/searchFilter/filterItems/Distance.js
+++ client/src/components/home/searchFilter/filterItems/Distance.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { InputNumber, Button } from 'antd';
+
+import { failNotification } from '../../../util/Notification';
+import {
+  loadingLocation,
+  editLocation,
+  getUserAddress
+} from '../../../../actions/user/location';
 
 //import LocationButton from '../../../createPost/confirmPost/location/GetLocationButton';
 
-export const Distance = () => {
+const Distance = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(state => state.location.isLoading);
+  const postalCode = useSelector(state => state.location.location.postalCode);
+
+  const handleGetLocation = () => {
+    dispatch(loadingLocation());
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        retrievedLocation,
+        showFailedNotification
+      );
+    } else {
+      showFailedNotification();
+    }
+  };
+
+  const retrievedLocation = location => {
+    const lat = location.coords.latitude;
+    const lng = location.coords.longitude;
+    dispatch(getUserAddress({ lat, lng }));
+  };
+
+  const showFailedNotification = () => {
+    failNotification('Could Not Load Location');
+  };
+
   return (
     <div>
       <strong>Distance: </strong>
@@ -17,14 +51,15 @@ export const Distance = () => {
       miles from{' '}
       <InputNumber
         size="small"
-        placeholder="area code"
+        placeholder={postalCode ? postalCode : 'area code'}
         formatter={value => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
         parser={value => value.replace(/\$\s?|(,*)/g, '')}
       />
       <Button
+        onClick={handleGetLocation}
         size="small"
         icon="environment"
-        loading={false}
+        loading={isLoading}
         type="primary"
         ghost
         style={{ display: 'inline-block', marginLeft: '5px' }}
