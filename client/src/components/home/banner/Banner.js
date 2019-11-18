83cba06d28200cada3c365e56e31546f6e1c25a9
David Dropping <ddropping@gmail.com>
2019-11-18 12:50:02 -0800

    Update home components css

--- client/src/components/home/banner/Banner.js
+++ client/src/components/home/banner/Banner.js
@@ -6,44 +6,46 @@ import './banner.css';
 
 const Banner = () => {
   return (
-    <Carousel>
-      <div>
-        <Link to="/">
-          <img
-            className="br-banner-img"
-            alt="br-logo-desktop"
-            src={process.env.PUBLIC_URL + '/images/br_banner_2.jpg'}
-          />
-        </Link>
-      </div>
-      <div>
-        <Link to="/">
-          <img
-            className="br-banner-img"
-            alt="br-logo-desktop"
-            src={process.env.PUBLIC_URL + '/images/br_banner_1.jpg'}
-          />
-        </Link>
-      </div>
-      <div>
-        <Link to="/">
-          <img
-            className="br-banner-img"
-            alt="br-logo-desktop"
-            src={process.env.PUBLIC_URL + '/images/br_banner_2.jpg'}
-          />
-        </Link>
-      </div>
-      <div>
-        <Link to="/">
-          <img
-            className="br-banner-img"
-            alt="br-logo-desktop"
-            src={process.env.PUBLIC_URL + '/images/br_banner_1.jpg'}
-          />
-        </Link>
-      </div>
-    </Carousel>
+    <div className="br-banner-carousel">
+      <Carousel>
+        <div>
+          <Link to="/">
+            <img
+              className="br-banner-img"
+              alt="br-logo-desktop"
+              src={process.env.PUBLIC_URL + '/images/br_banner_2.jpg'}
+            />
+          </Link>
+        </div>
+        <div>
+          <Link to="/">
+            <img
+              className="br-banner-img"
+              alt="br-logo-desktop"
+              src={process.env.PUBLIC_URL + '/images/br_banner_1.jpg'}
+            />
+          </Link>
+        </div>
+        <div>
+          <Link to="/">
+            <img
+              className="br-banner-img"
+              alt="br-logo-desktop"
+              src={process.env.PUBLIC_URL + '/images/br_banner_2.jpg'}
+            />
+          </Link>
+        </div>
+        <div>
+          <Link to="/">
+            <img
+              className="br-banner-img"
+              alt="br-logo-desktop"
+              src={process.env.PUBLIC_URL + '/images/br_banner_1.jpg'}
+            />
+          </Link>
+        </div>
+      </Carousel>
+    </div>
   );
 };
 
