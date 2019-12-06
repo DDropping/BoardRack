5da12551c23e783f1d2d2e26d612670abea14b04
David Dropping <ddropping@gmail.com>
2019-12-05 18:06:03 -0800

    Update Dimensions component to use layout state

--- client/src/components/post/Dimensions.js
+++ client/src/components/post/Dimensions.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Dimensions = ({ lengthFt, lengthIn, width, depth, volume }) => {
+  const layout = useSelector(state => state.filters.layout);
+
   return (
     <div style={{ fontStyle: 'italic', display: 'inline-block' }}>
       <div style={{ display: 'inline-block' }}>
-        {lengthFt && 'Dims: ' + lengthFt + "' "}
+        {layout !== 'Gallery' && lengthFt && 'Dims: '}
+        {lengthFt && lengthFt + "' "}
         {lengthIn && lengthIn + '" '}
         {(lengthFt || lengthIn) && 'x '}
         {width && width + '" x '}
         {depth && depth + '"'}
       </div>
       <div style={{ display: 'inline-block', marginLeft: '10px' }}>
+        {layout !== 'Gallery' && volume && 'Volume: '}
         {volume && volume + 'L'}
       </div>
     </div>
