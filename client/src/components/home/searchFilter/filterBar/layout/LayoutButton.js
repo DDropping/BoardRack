0965e5a890f0179dd8a1ff9c9e41d9dd780f41a6
David Dropping <ddropping@gmail.com>
2020-01-01 22:57:55 -0800

    Update layout button to use antd dropdown instead of custom code

--- client/src/components/home/searchFilter/filterBar/layout/LayoutButton.js
+++ client/src/components/home/searchFilter/filterBar/layout/LayoutButton.js
@@ -1,67 +1,68 @@
-import React, { useState, useCallback, useRef } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Icon, Button } from 'antd';
+import { Icon, Menu, Dropdown } from 'antd';
 
 import './layoutButton.css';
-import { useOnClickOutside } from '../../../../../hooks/onClickOutside';
 import { UPDATE_LAYOUT } from '../../../../../actions/types';
 
 const LayoutButton = () => {
   const dispatch = useDispatch();
   const layoutValue = useSelector(state => state.filters.layout);
-  const [isVisible, setIsVisible] = useState(false);
-
-  const ref = useRef();
-  useOnClickOutside(
-    ref,
-    useCallback(() => setIsVisible(false), [])
-  );
 
   return (
-    <div className="br-layout-button" ref={ref}>
-      <Button
-        onClick={() => setIsVisible(!isVisible)}
-        style={{ marginRight: '30px' }}
-        type="link"
+    <div
+      style={{
+        display: 'inline-block',
+        float: 'right'
+      }}
+    >
+      <Dropdown
+        placement="bottomCenter"
+        trigger={['click']}
+        overlay={
+          <Menu>
+            <Menu.Item>
+              <div
+                onClick={() =>
+                  dispatch({ type: UPDATE_LAYOUT, payload: 'List' })
+                }
+              >
+                <Icon type="menu" />
+                {' List'}
+              </div>
+            </Menu.Item>
+            <Menu.Item>
+              <div
+                onClick={() =>
+                  dispatch({ type: UPDATE_LAYOUT, payload: 'Thumb' })
+                }
+              >
+                <Icon type="unordered-list" />
+                {' Thumb'}
+              </div>
+            </Menu.Item>
+            <Menu.Item>
+              <div
+                onClick={() =>
+                  dispatch({ type: UPDATE_LAYOUT, payload: 'Gallery' })
+                }
+              >
+                <Icon type="appstore" />
+                {' Gallery'}
+              </div>
+            </Menu.Item>
+          </Menu>
+        }
       >
-        {layoutValue === 'List' && <Icon type="menu" />}
-        {layoutValue === 'Thumb' && <Icon type="unordered-list" />}
-        {layoutValue === 'Gallery' && <Icon type="appstore" />}
-        {layoutValue}
-      </Button>
-      {isVisible && (
-        <div className="br-layout-options">
-          <Button
-            onClick={() => {
-              dispatch({ type: UPDATE_LAYOUT, payload: 'List' });
-              setIsVisible(false);
-            }}
-            type="link"
-          >
-            <Icon type="menu" /> List
-          </Button>
-          <br />
-          <Button
-            onClick={() => {
-              dispatch({ type: UPDATE_LAYOUT, payload: 'Thumb' });
-              setIsVisible(false);
-            }}
-            type="link"
-          >
-            <Icon type="unordered-list" /> Thumb
-          </Button>
-          <br />
-          <Button
-            onClick={() => {
-              dispatch({ type: UPDATE_LAYOUT, payload: 'Gallery' });
-              setIsVisible(false);
-            }}
-            type="link"
-          >
-            <Icon type="appstore" /> Gallery
-          </Button>
+        <div style={{ cursor: 'pointer' }}>
+          <strong>
+            {layoutValue === 'List' && <Icon type="menu" />}
+            {layoutValue === 'Thumb' && <Icon type="unordered-list" />}
+            {layoutValue === 'Gallery' && <Icon type="appstore" />}
+            {' ' + layoutValue} <Icon type="down" />
+          </strong>
         </div>
-      )}
+      </Dropdown>
     </div>
   );
 };
