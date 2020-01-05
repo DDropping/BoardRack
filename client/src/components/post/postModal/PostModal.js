0694aa9c4433ee4b6b2183b622c9be97b957a70c
David Dropping <ddropping@gmail.com>
2020-01-04 16:41:20 -0800

    Update postModal refactor

--- client/src/components/post/postModal/PostModal.js
+++ client/src/components/post/postModal/PostModal.js
@@ -1,13 +1,28 @@
 /* Displays postModal if isPostModalVisible === true */
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Fade from 'react-reveal/Fade';
 
-import '../post.css';
+import './postModal.css';
+import { addView } from '../../../actions/post/post';
 import PostModalToolBar from './PostModalToolBar';
 import PostModalBody from './PostModalBody';
 
 const PostModal = ({ post, hidePostModal }) => {
+  const dispatch = useDispatch();
+  const viewedPosts = useSelector(state => state.post.viewedPosts);
+
+  useEffect(() => {
+    //increase post viewCount if this is first time user is viewing post
+    if (
+      viewedPosts.filter(postId => postId.toString() === post._id.toString())
+        .length === 0
+    ) {
+      dispatch(addView(post._id));
+    }
+  }, [post._id, dispatch, viewedPosts]);
+
   return (
     <Fade>
       <div className="br-post-modal-grey" onClick={() => hidePostModal()} />
--- client/src/components/post/postModal/PostModalToolBar.js
+++ client/src/components/post/postModal/PostModalToolBar.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Icon } from 'antd';
 
+import './postModal.css';
+
 const PostModalToolBar = ({ post, hidePostModal }) => {
   return (
-    <div>
-      <Icon
-        type="close-circle"
-        onClick={() => hidePostModal()}
-        style={{
-          fontSize: '25px',
-          position: 'absolute',
-          right: 5,
-          top: 5
-        }}
-      />
+    <div className="br-post-modal-toolbar-wrapper">
+      <div className="br-post-modal-toolbar">
+        <Icon
+          type="close-circle"
+          onClick={() => hidePostModal()}
+          style={{
+            fontSize: '25px',
+            position: 'absolute',
+            right: 5,
+            top: 5
+          }}
+        />
+      </div>
     </div>
   );
 };
