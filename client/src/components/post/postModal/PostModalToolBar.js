375a363b99bff6fa64aae93e6db1649018f37707
David Dropping <ddropping@gmail.com>
2020-01-04 18:54:35 -0800

    Create outline for post modal toolbar

--- client/src/components/post/postModal/PostModalToolBar.js
+++ client/src/components/post/postModal/PostModalToolBar.js
@@ -6,18 +6,39 @@ import './postModal.css';
 const PostModalToolBar = ({ post, hidePostModal }) => {
   return (
     <div className="br-post-modal-toolbar-wrapper">
-      <div className="br-post-modal-toolbar">
-        <Icon
-          type="close-circle"
-          onClick={() => hidePostModal()}
-          style={{
-            fontSize: '25px',
-            position: 'absolute',
-            right: 5,
-            top: 5
-          }}
-        />
+      <div
+        style={{
+          display: 'inline-block',
+          padding: '0px 20px',
+          cursor: 'pointer'
+        }}
+        onClick={() => console.log('click favorite postmodaltoolbar')}
+      >
+        <Icon type="star" theme="filled" /> Favorite
       </div>
+      <div
+        style={{
+          display: 'inline-block',
+          padding: '0px 20px',
+          cursor: 'pointer',
+          borderLeft: '2px solid white'
+        }}
+        onClick={() => console.log('click favorite postmodaltoolbar')}
+      >
+        <Icon type="mail" /> Contact
+      </div>
+      <div
+        style={{
+          float: 'right',
+          padding: '0px 20px',
+          cursor: 'pointer',
+          borderLeft: '2px solid white'
+        }}
+        onClick={() => hidePostModal()}
+      >
+        <Icon type="close-circle" /> Close
+      </div>
+      <div className="br-post-modal-toolbar"></div>
     </div>
   );
 };
