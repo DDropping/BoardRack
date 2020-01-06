ee6fed44e493753b0115b40079c9a05eb51afc36
David Dropping <ddropping@gmail.com>
2020-01-06 09:09:46 -0800

    Create scroll bar for images

--- client/src/components/post/postModal/PostModalImages.js
+++ client/src/components/post/postModal/PostModalImages.js
@@ -5,14 +5,16 @@ import './postModal.css';
 const PostModalImages = ({ images }) => {
   return (
     <div className="br-post-modal-images-wrapper">
-      {images.map((image, index) => (
-        <img
-          style={{ maxWidth: '100%', marginBottom: '2px' }}
-          key={index}
-          alt=""
-          src={image.default}
-        />
-      ))}
+      <div className="br-post-modal-images">
+        {images.map((image, index) => (
+          <img
+            style={{ maxWidth: '100%', marginBottom: '2px' }}
+            key={index}
+            alt=""
+            src={image.default}
+          />
+        ))}
+      </div>
     </div>
   );
 };
