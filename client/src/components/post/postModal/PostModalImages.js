16a9447a59ebe935e5959655e557c84937976566
David Dropping <ddropping@gmail.com>
2020-03-03 10:30:25 -0800

    Update postModalImages to stretch width 100%

--- client/src/components/post/postModal/PostModalImages.js
+++ client/src/components/post/postModal/PostModalImages.js
@@ -8,7 +8,7 @@ const PostModalImages = ({ images }) => {
       <div className="br-post-modal-images">
         {images.map((image, index) => (
           <img
-            style={{ maxWidth: '100%', marginBottom: '2px' }}
+            style={{ width: '100%', marginBottom: '2px' }}
             key={index}
             alt=""
             src={image.default}
