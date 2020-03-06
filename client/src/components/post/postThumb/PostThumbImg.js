c7c794ed95c6a444c61fde924e65764647e1e77e
David Dropping <ddropping@gmail.com>
2020-03-06 11:44:45 -0800

    update post thumb image css

--- client/src/components/post/postThumb/PostThumbImg.js
+++ client/src/components/post/postThumb/PostThumbImg.js
@@ -5,6 +5,10 @@ import './postThumb.css';
 const PostThumbImg = ({ post }) => {
   return (
     <div className="br-postThumb-img-container">
+      <div
+        className="br-postThumb-img-background"
+        style={{ backgroundImage: 'url(' + post.images[0].thumbnail + ')' }}
+      />
       <img
         className="br-postThumb-img"
         alt="surfboard"
