5386ad2a64943ba2becfc366e71720a7442212f3
David Dropping <ddropping@gmail.com>
2020-03-06 10:19:39 -0800

    Update post modal footer image

--- client/src/components/post/postModal/PostModalDetails/PostModalFooter.js
+++ client/src/components/post/postModal/PostModalDetails/PostModalFooter.js
@@ -6,7 +6,7 @@ const PostModalFooter = () => {
       <img
         alt="decoritive surfboard footer"
         style={{ maxWidth: '100%', height: 'auto' }}
-        src={process.env.PUBLIC_URL + '/images/br_board_outlines_blue.png'}
+        src={process.env.PUBLIC_URL + '/images/br-surfboard-types-outlines.png'}
       ></img>
     </div>
   );
